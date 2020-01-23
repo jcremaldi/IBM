eefb6f280f5b5c353d0ff6a356fd34c8f5cb9481
jcremaldi <30802497+jcremaldi@users.noreply.github.com>
2020-01-23 09:09:12 -0600

    Add files via upload

--- IBM_v2.py
+++ IBM_v2.py
@@ -21,11 +21,13 @@ from sklearn import svm
 # Set it to None to display all columns in the dataframe
 pd.set_option('display.max_columns', None)
 
-df = pd.read_csv('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_train.csv')
+raw_data = pd.read_csv('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_train.csv')
+df = raw_data.copy()
 df = df.drop(['Unnamed: 0','Unnamed: 0.1'],axis=1)
+print(df.describe())
+
  
 def data_prep(df):
-    
     df['due_date'] = pd.to_datetime(df['due_date'])
     df['effective_date'] = pd.to_datetime(df['effective_date'])
     df['dayofweek'] = df['effective_date'].dt.dayofweek
@@ -33,7 +35,7 @@ def data_prep(df):
     df['Gender'].replace(to_replace=['male','female'], value=[0,1],inplace=True)
     
     #one hot encode the education level, why do they drop 'Master or Above'?
-    temp = df[['Principal','terms','age','Gender','weekend']]
+    temp = df[['Principal','terms','age','Gender','weekend']].copy()
     temp = pd.concat([temp,pd.get_dummies(df['education'])], axis=1)
     temp = pd.concat([temp,pd.get_dummies(df['terms'])], axis=1)
     temp.drop(['Master or Above','terms'], axis = 1,inplace=True)
@@ -46,8 +48,6 @@ def data_prep(df):
     scaler = preprocessing.StandardScaler().fit(ss_col.values)
     features = scaler.transform(ss_col.values)
     temp[ss_col_names] = features
-    print(temp.head(10))
-
     X = temp
     y = df['loan_status'].values
     return X, y
@@ -60,18 +60,18 @@ def plot_trend(n, mean_acc, x_title):
     plt.show()    
 
 def strat_k_folds(model_skfold,X, y):
-    skfold = model_selection.StratifiedKFold(n_splits=5)
+    skfold = model_selection.StratifiedKFold(n_splits = 4)
     results_skfold = model_selection.cross_val_score(model_skfold, X, y, cv=skfold)
     return results_skfold.mean()
 
 X, y = data_prep(df)
 #------------------------------------------------------------------------------
-Ks = 25
+Ks = 20
 mean_acc_knn = np.zeros((Ks-1))
 
-for n in range(1,Ks):
-    neigh = KNeighborsClassifier(n_neighbors = n)    
-    mean_acc_knn[n-1] = strat_k_folds(neigh, X, y)
+for n_knn in range(1,Ks):
+    neigh = KNeighborsClassifier(n_neighbors = n_knn)    
+    mean_acc_knn[n_knn-1] = strat_k_folds(neigh, X, y)
 
 plot_trend(Ks, mean_acc_knn, 'Number of Neighbors (K)')
 best_n = mean_acc_knn.argmax()+1
@@ -80,10 +80,10 @@ print( "The best accuracy was ", mean_acc_knn.max(), "with k=", best_n, '\n')
 dep = 15
 mean_acc_dt = np.zeros((dep-1))
 
-for n in range(1,dep):
+for n_dep in range(1,dep):
     #Train Model and Predict  
-    loanTree = DecisionTreeClassifier(criterion="entropy", max_depth = n)
-    mean_acc_dt[n-1] = strat_k_folds(loanTree, X, y) 
+    loanTree = DecisionTreeClassifier(criterion="entropy", max_depth = n_dep)
+    mean_acc_dt[n_dep-1] = strat_k_folds(loanTree, X, y) 
 
 plot_trend(dep, mean_acc_dt, 'Max Tree Depth')
 best_dep = mean_acc_dt.argmax()+1
@@ -126,52 +126,90 @@ best_c = c_test[mean_acc_lr.argmax()+1]
 print( "The best accuracy was ", mean_acc_lr.max(), "with c = ", best_c, '\n')     
 
 #------------------------------------------------------------------------------
-#train models using optimal parameters and full data set
-neigh_pred = KNeighborsClassifier(n_neighbors = best_n)
-neigh_pred.fit(X, y)
-loanTree_pred = DecisionTreeClassifier(criterion="entropy", max_depth = best_dep)
-loanTree_pred.fit(X, y)
-clf_pred = svm.SVC(kernel=str(temp_max_svm))
-clf_pred.fit(X, y)
-LR_pred = LogisticRegression(C=best_c, solver=str(temp_max_lr))
-LR_pred.fit(X, y)
+df_train = raw_data.copy()
+df_train = df_train.drop(['Unnamed: 0','Unnamed: 0.1'],axis=1)
+
+X_train, y_train = data_prep(df_train)
 
 #apply the same transformations to the data as used in building the model
 df_test =  pd.read_csv('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_test.csv')
 df_test = df_test.drop(['Unnamed: 0','Unnamed: 0.1'],axis=1)
 
 X_test, y_test = data_prep(df_test)
-
+#------------------------------------------------------------------------------
+#train models using optimal parameters and full data set
+neigh_pred = KNeighborsClassifier(n_neighbors = best_n).fit(X_train, y_train)
 predNeigh = neigh_pred.predict(X_test)
-predLoanTree = loanTree_pred.predict(X_test)
-predClf = clf_pred.predict(X_test)
-predLR = LR_pred.predict(X_test)
-predLR_prob = LR_pred.predict_proba(X_test)
-
 print('\n')
 print('KNeighborsClassifier')
 print('accuracy_score: ', metrics.accuracy_score(y_test, predNeigh))
 print('f1_score: ', metrics.f1_score(y_test, predNeigh, average='weighted'))
 print('jaccard_score: ',metrics.jaccard_score(y_test, predNeigh, average='weighted'))
 
-print('\n')
-print('DecisionTreeClassifier')
-print('accuracy_score: ', metrics.accuracy_score(y_test, predLoanTree))
-print('f1_score: ',metrics.f1_score(y_test, predLoanTree, average='weighted'))
-print('jaccard_score: ',metrics.jaccard_score(y_test, predLoanTree, average='weighted'))
 
+clf_pred = svm.SVC(kernel=str(temp_max_svm)).fit(X_train, y_train)
+predClf = clf_pred.predict(X_test)
 print('\n')
 print('svm')
 print('accuracy_score: ', metrics.accuracy_score(y_test, predClf))
 print('f1_score: ',metrics.f1_score(y_test, predClf, average='weighted'))
 print('jaccard_score: ',metrics.jaccard_score(y_test, predClf, average='weighted'))
 
+
+loanTree_pred = DecisionTreeClassifier(criterion="entropy", max_depth = best_dep).fit(X_train, y_train)
+predLoanTree = loanTree_pred.predict(X_test)
+print('\n')
+print('DecisionTreeClassifier')
+print('accuracy_score: ', metrics.accuracy_score(y_test, predLoanTree))
+print('f1_score: ',metrics.f1_score(y_test, predLoanTree, average='weighted'))
+print('jaccard_score: ',metrics.jaccard_score(y_test, predLoanTree, average='weighted'))
+
+
+LR_pred = LogisticRegression(C=best_c, solver=str(temp_max_lr)).fit(X_train, y_train)
+predLR = LR_pred.predict(X_test)
+predLR_prob = LR_pred.predict_proba(X_test)
 print('\n')
 print('LogisticRegression')
 print('accuracy_score: ', metrics.accuracy_score(y_test, predLR))
 print('f1_score: ',metrics.f1_score(y_test, predLR, average='weighted'))
 print('jaccard_score: ',metrics.jaccard_score(y_test, predLR, average='weighted'))
 print('log_loss: ',metrics.log_loss(y_test, predLR_prob))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
